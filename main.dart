19d482f3727979f86f00171c77173be2fcdea8df
farhadnoori442260 <farhadnoori552@gmail.com>
2025-08-12 14:22:09 +0330

    Add files via upload
    Add new version of project files

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Taxi App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'B_Nazanin', // فونت پیش‌فرض کل برنامه
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
